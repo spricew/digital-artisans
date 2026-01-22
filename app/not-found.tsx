4dd371aa0db3f6ff01c81ca146cbb53150bcd70a
Heyder Medina <medinaheyder@gmail.com>
2026-01-21 23:07:35 -0600

    style: enhance 404 page with new background color, text color, and button styling; add ChevronRight icon

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,16 @@
+
+import { ChevronRight } from 'lucide-react';
 import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className='flex flex-col justify-center items-center gap-4 w-full min-h-screen bg-zinc-900'>
-      <h2 className='text-6xl font-medium'>404 - Page Not Found</h2>
+    <div className='flex flex-col justify-center items-center gap-4 w-full min-h-screen bg-[#18120c]'>
+      <h2 className='text-6xl font-medium text-[#ffddbb]'>404 - Page Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/" className='inline-flex py-1 px-3 rounded-full bg-amber-600'>Return Home</Link>
+      <Link href="/" className='inline-flex py-2 px-4 rounded-full bg-[#482900] font-semibold text-[#ffddbb]'>
+      Return Home
+      <ChevronRight size={25} strokeWidth={2.2} />
+      </Link>
     </div>
   );
 }
