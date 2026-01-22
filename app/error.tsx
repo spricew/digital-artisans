f9d64065220f6a926ffc3a4fe0c957bafb66189d
Heyder Medina <medinaheyder@gmail.com>
2026-01-22 00:10:18 -0600

    feat: implement GlobalError component to handle and display error messages with user-friendly information

---
+++ app/error.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { Info } from "lucide-react";
+import { CircleQuestionMark } from "lucide-react";
+
+const ERROR_CODES = {
+    400: { title: "Bad Request", simple: "Petición mal formada", blame: "Usuario / Navegador" },
+    401: { title: "Unauthorized", simple: "Falta iniciar sesión", blame: "Usuario" },
+    403: { title: "Forbidden", simple: "No tienes permisos", blame: "Usuario (Nivel de acceso)" },
+    500: { title: "Internal Error", simple: "El código falló", blame: "Servidor (Desarrollador)" },
+    502: { title: "Bad Gateway", simple: "Error de comunicación", blame: "Servidor (Infraestructura)" },
+    503: { title: "Unavailable", simple: "Sobrecarga / Mantenimiento", blame: "Servidor" },
+    504: { title: "Timeout", simple: "Se tardó demasiado", blame: "Servidor Objetivo" },
+};
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error
+    reset: () => void
+}) {
+
+    const errorCode = Object.keys(ERROR_CODES).find(code => error.message.includes(code)) || 500;
+
+    // @ts-ignore - se ignora el typescript por el indexado de string
+    const info = ERROR_CODES[errorCode];
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-4 h-screen">
+            <h2 className='text-6xl font-medium text-onPrimaryContainer'>{errorCode} - {info.title}</h2>
+
+            <div className="flex items-center justify-center gap-4 w-full">
+
+                <div className="squircle flex justify-start items-center gap-3 p-6 min-w-fit w-64 h-24 rounded-xl bg-primaryContainer">
+                    <CircleQuestionMark className="size-10 text-onPrimaryContainer" strokeWidth={2.2} />
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-semibold text-onPrimaryContainer">Significado:</span>
+                        <p className="">{info.simple}</p>
+                    </div>
+                </div>
+
+                <div className="squircle flex justify-start items-center gap-3 p-6 min-w-fit w-64 h-24 rounded-xl bg-primaryContainer">
+                    <Info className="size-10 text-onPrimaryContainer" strokeWidth={2.2} />
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-semibold text-onPrimaryContainer">La culpa es de:</span>
+                        <p className="">{info.blame}</p>
+                    </div>
+                </div>
+
+            </div>
+        
+
+            <button
+                onClick={reset}
+                className="mt-2 px-6 py-2 bg-white text-black rounded hover:bg-gray-200 transition"
+            >
+                Intentar de nuevo
+            </button>
+        </div>
+    )
+}
