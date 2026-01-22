248675718154a5a13703b14e5829f9706d9516ad
Heyder Medina <medinaheyder@gmail.com>
2026-01-21 18:53:35 -0600

    feat: update layout for the 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,11 @@
-// app/not-found.tsx
 import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className='flex flex-col justify-center items-center gap-4 w-full min-h-screen bg-zinc-900'>
+      <h2 className='text-6xl font-medium'>404 - Page Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/" className='inline-flex py-1 px-3 rounded-full bg-amber-600'>Return Home</Link>
     </div>
   );
 }
