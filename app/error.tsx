b612e2e8495a1115c7114275863e6f806985e48a
Heyder Medina <medinaheyder@gmail.com>
2026-01-22 12:10:41 -0600

    feat: refactor GlobalError component to utilize external error codes and enhance error display with new SVG images

--- app/error.tsx
+++ app/error.tsx
@@ -1,16 +1,6 @@
 'use client'
-
+import { codes } from '@/lib/data/errorCodes';
 import { Info, CircleQuestionMark, RotateCcw } from 'lucide-react';
-
-const ERROR_CODES = {
-    400: { title: "Bad Request", simple: "Petición mal formada", blame: "Usuario / Navegador" },
-    401: { title: "Unauthorized", simple: "Falta iniciar sesión", blame: "Usuario" },
-    403: { title: "Forbidden", simple: "No tienes permisos", blame: "Usuario (Nivel de acceso)" },
-    500: { title: "Internal Error", simple: "El código falló", blame: "Servidor (Desarrollador)" },
-    502: { title: "Bad Gateway", simple: "Error de comunicación", blame: "Servidor (Infraestructura)" },
-    503: { title: "Unavailable", simple: "Sobrecarga / Mantenimiento", blame: "Servidor" },
-    504: { title: "Timeout", simple: "Se tardó demasiado", blame: "Servidor Objetivo" },
-};
 
 export default function GlobalError({
     error,
@@ -20,13 +10,18 @@ export default function GlobalError({
     reset: () => void
 }) {
 
-    const errorCode = Object.keys(ERROR_CODES).find(code => error.message.includes(code)) || 500;
+    const errorCode = Object.keys(codes).find(code => error.message.includes(code)) || 500;
 
     // @ts-ignore - se ignora el typescript por el indexado de string
-    const info = ERROR_CODES[errorCode];
+    const info = codes[errorCode];
 
     return (
         <div className="flex flex-col items-center justify-center gap-4 h-screen">
+
+            <div className='flex items-center justify-center p-6 size-64 rounded-full bg-surfaceContainerHigh'>
+                <img src={info.image.src} className='w-full h-full object-center object-cover'/>
+            </div>
+
             <h2 className='text-6xl font-medium text-onPrimaryContainer'>{errorCode} - {info.title}</h2>
 
             <div className="flex items-center justify-center gap-4 w-full">
@@ -35,7 +30,7 @@ export default function GlobalError({
                     <CircleQuestionMark className="size-10 text-onPrimaryContainer" strokeWidth={2.2} />
                     <div className="flex flex-col">
                         <span className="text-2xl font-semibold text-onPrimaryContainer">Significado:</span>
-                        <p className="">{info.simple}</p>
+                        <p className="">{info.reason}</p>
                     </div>
                 </div>
 
@@ -53,10 +48,10 @@ export default function GlobalError({
             <button
                 onClick={reset}
                 className="flex items-center justify-center gap-3 px-5 py-2.5 mt-2 rounded-full
-                 bg-primaryContainer text-onPrimaryContainer
+                 bg-primaryContainer text-onPrimaryContainer font-semibold
                 transition ease-out duration-300 hover:scale-105 cursor-pointer"
             >
-                <RotateCcw className="size-5 text-onPrimaryContainer" strokeWidth={2.2} />
+                <RotateCcw className="size-5 text-onPrimaryContainer" strokeWidth={2.8} />
                 Intentar de nuevo
             </button>
         </div>
