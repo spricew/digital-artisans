b66b03f2a8308a10c33c6f4296488a6416e0ed7a
Heyder Medina <medinaheyder@gmail.com>
2026-01-22 12:00:53 -0600

    feat: enhance 404 page with new error image and updated text in Spanish for better user engagement

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,27 @@
 import type { Metadata } from "next";
 
+// titulo de la pestaña jeje
 export const metadata: Metadata = {
   title: "Not Found",
 };
 
-
-import { ChevronRight } from 'lucide-react';
 import Link from 'next/link';
+import { ChevronRight } from 'lucide-react';
+import ErrorImg from "./components/ui/errorImg";
+import notFoundImg from '@/public/notFound.png'
 
 export default function NotFound() {
   return (
     <div className='flex flex-col justify-center items-center gap-6 w-full min-h-screen bg-[#18120c]'>
+      <ErrorImg imagesrc={notFoundImg} />
       <h2 className='text-6xl font-medium text-onPrimaryContainer'>404 - Page Not Found</h2>
-      <p>Página no encontrada.</p>
+      <p className="squircle max-w-2xl text-center text-balance leading-7 p-6 bg-surfaceContainerHigh rounded-lg">
+        Ups... no pudimos encontrar la página que buscas. Es posible que la dirección esté
+        incorrecta o que la página haya sido movida.
+      </p>
       <Link href="/" className='inline-flex py-2 px-4 rounded-full bg-onPrimary font-semibold text-onPrimaryContainer'>
-      Return Home
-      <ChevronRight size={25} strokeWidth={2.2} />
+        Return Home
+        <ChevronRight size={25} strokeWidth={2.2} />
       </Link>
     </div>
   );
