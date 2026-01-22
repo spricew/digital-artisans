98471c17951c7664c16e3e9f441f1877d25c042d
Heyder Medina <medinaheyder@gmail.com>
2026-01-22 12:51:00 -0600

    feat: add PrimaryButton component for flexible button rendering with optional link and icon support

---
+++ app/components/ui/primaryButton.tsx
@@ -0,0 +1,38 @@
+
+import { ElementType, ReactNode } from "react";
+import Link from 'next/link';
+
+interface ButtonProps {
+    text: string;
+    IconBtn: ElementType;
+    href?: string;
+    onClick?: () => void;
+    extraclass?: string;
+}
+
+export default function PrimaryButton({ text, href, IconBtn, onClick, extraclass }: ButtonProps) {
+
+    const baseStyles = `flex items-center justify-center gap-2 px-5 py-2.5 rounded-full
+                 bg-primaryContainer text-onPrimaryContainer font-semibold
+                transition ease-out duration-300 hover:scale-105 cursor-pointer ${extraclass}`;
+
+    return (
+        <>
+            {href ?
+                (
+                    <Link href={href} className={`${baseStyles}`}>
+                        {text}
+                        <IconBtn className="size-5 text-onPrimaryContainer" strokeWidth={3} />
+                    </Link>
+                ) : (
+                    <button className={`${baseStyles}`}
+                        onClick={onClick}>
+
+                        <IconBtn className="size-5 text-onPrimaryContainer" strokeWidth={3} />
+                        {text}
+
+                    </button>)}
+
+        </>
+    );
+}
