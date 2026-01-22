002dcbdf238cb64b15e0605ee7567c9844ba486e
Heyder Medina <medinaheyder@gmail.com>
2026-01-21 23:31:26 -0600

    refactor: update text and button colors on 404 page to align with new design system

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className='flex flex-col justify-center items-center gap-4 w-full min-h-screen bg-[#18120c]'>
-      <h2 className='text-6xl font-medium text-[#ffddbb]'>404 - Page Not Found</h2>
+      <h2 className='text-6xl font-medium text-onPrimaryContainer'>404 - Page Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/" className='inline-flex py-2 px-4 rounded-full bg-[#482900] font-semibold text-[#ffddbb]'>
+      <Link href="/" className='inline-flex py-2 px-4 rounded-full bg-onPrimary font-semibold text-onPrimaryContainer'>
       Return Home
       <ChevronRight size={25} strokeWidth={2.2} />
       </Link>
