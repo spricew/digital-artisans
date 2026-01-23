2c2a2bb35745d645d56df673829008d734bf864e
Heyder Medina <medinaheyder@gmail.com>
2026-01-22 20:19:48 -0600

    feat: update error handling in Dashboard and enhance errorCodes with additional images for better user feedback

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ const BASE_URL = 'https://pokeapi.co/api/v2/pokemonasd/ditto';
 export default async function Dashboard() {
   const res = await fetch(`${BASE_URL}`);
 
-  if (!res.ok) {
-    throw new Error(`${res.status}`);
-  }
+  // if (!res.ok) {
+  //   throw new Error(`${res.status}`);
+  // }
+
+  throw new Error('400');
 
   return (
     <div className="flex items-center justify-center w-full h-screen text-3xl text-onPrimaryContainer">
--- lib/data/errorCodes.ts
+++ lib/data/errorCodes.ts
@@ -1,12 +1,17 @@
-import badRequest from '@/public/badRequest.svg'
-
+import badRequest from '@/public/badRequest.svg';
+import unautorized from '@/public/unauthorized.png';
+import forbidden from '@/public/forbidden.png';
+import internalError from '@/public/internalError.png';
+import badGateway from '@/public/badGateway.png';
+import unavailable from '@/public/unavailable.png';
+import timeout from '@/public/timeout.png';
 
 export const codes = {
     400: { title: "Bad Request", reason: "Petición mal formada", blame: "Usuario / Navegador", image: badRequest },
-    401: { title: "Unauthorized", reason: "Falta iniciar sesión", blame: "Usuario", image: badRequest },
-    403: { title: "Forbidden", reason: "No tienes permisos", blame: "Usuario (Nivel de acceso)", image: badRequest },
-    500: { title: "Internal Error", reason: "El código falló", blame: "Servidor (Desarrollador)", image: badRequest },
-    502: { title: "Bad Gateway", reason: "Error de comunicación", blame: "Servidor (Infraestructura)", image: badRequest },
-    503: { title: "Unavailable", reason: "Sobrecarga / Mantenimiento", blame: "Servidor", image: badRequest },
-    504: { title: "Timeout", reason: "Se tardó demasiado", blame: "Servidor Objetivo", image: badRequest },
+    401: { title: "Unauthorized", reason: "Falta iniciar sesión", blame: "Usuario", image: unautorized },
+    403: { title: "Forbidden", reason: "No tienes permisos", blame: "Usuario (Nivel de acceso)", image: forbidden },
+    500: { title: "Internal Error", reason: "El código falló", blame: "Servidor (Desarrollador)", image: internalError },
+    502: { title: "Bad Gateway", reason: "Error de comunicación", blame: "Servidor (Infraestructura)", image: badGateway },
+    503: { title: "Unavailable", reason: "Sobrecarga / Mantenimiento", blame: "Servidor", image: unavailable },
+    504: { title: "Timeout", reason: "Se tardó demasiado", blame: "Servidor Objetivo", image: timeout },
 };
