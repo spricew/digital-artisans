a9183f63efef3af309a26032e1e49e56cc0a63e8
Heyder Medina <medinaheyder@gmail.com>
2026-01-24 16:56:07 -0600

    refactor: update NotFound component layout and styles for improved visual consistency and user experience

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,10 +12,10 @@ import PrimaryButton from "./components/ui/PrimaryButton";
 
 export default function NotFound() {
   return (
-    <div className='flex flex-col justify-center items-center gap-6 w-full min-h-screen bg-[#18120c]'>
-      <ErrorImg imagesrc={notFoundImg} />
+    <div className='flex flex-col justify-center items-center gap-6 w-full h-full'>
+      {/* <ErrorImg imagesrc={notFoundImg} /> */}
       <h2 className='text-6xl font-medium text-onPrimaryContainer'>404 - Page Not Found</h2>
-      <p className="squircle max-w-2xl text-center text-balance leading-7 p-6 bg-surfaceContainerHigh rounded-lg">
+      <p className="squircle max-w-2xl text-xl text-center text-balance leading-7 p-6 text-onSurface bg-surfaceContainerHigh rounded-lg">
         Ups... no pudimos encontrar la página que buscas. Es posible que la dirección esté
         incorrecta o que la página haya sido movida.
       </p>
