50e4e1cdc89d293e5b7c629b5684cd0f7a868f77
Heyder Medina <medinaheyder@gmail.com>
2026-01-25 16:09:51 -0600

    feat: enhance NotFound component with visual elements for improved user experience

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,6 +11,12 @@ import PrimaryButton from "./components/ui/PrimaryButton";
 export default function NotFound() {
   return (
     <div className='flex flex-col justify-center items-center gap-3 w-full h-full'>
+      <div className="flex items-center font-bold text-[11rem] text-onSurfaceVariant">
+        4
+          <img src="/notFoundImg.png" alt="Art Palette" className="h-50 object-cover -mx-10" />
+          4
+      </div>
+
       <h2 className='text-6xl font-bold tracking-tighter text-onBackground px-5'>Página no encontrada...</h2>
       <p className="squircle max-w-xl p-6 rounded-lg text-pretty text-xl text-center leading-7 
        text-onSurface bg-surfaceContainerHigh">
