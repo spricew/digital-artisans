70506b40e587b3683d6208b5f03394a3731708ec
Heyder Medina <medinaheyder@gmail.com>
2026-01-25 16:25:27 -0600

    refactor: home page cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-// const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/ditto';
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemonasd/ditto';
 
-// app/dashboard/page.tsx
 export default async function Dashboard() {
-  const res = await fetch(`${BASE_URL}`);
 
-  // if (!res.ok) {
-  //   throw new Error(`${res.status}`);
-  // }
-
-  throw new Error('400');
+  // throw new Error('500');
 
   return (
-    <div className="flex items-center justify-center w-full h-screen text-3xl text-onPrimaryContainer">
-      Bienvenido
+    <div className="flex items-center justify-center w-full h-full text-3xl text-onPrimaryContainer">
     </div>
   );
 }
