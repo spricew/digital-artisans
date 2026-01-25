df950242d1f9490260761a38e0edcc0eb81c5e01
Heyder Medina <medinaheyder@gmail.com>
2026-01-25 14:23:34 -0600

    refactor: remove unused ErrorImg component and notFound image import from NotFound component for cleaner code

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,14 +6,11 @@ export const metadata: Metadata = {
 };
 
 import { ChevronRight } from 'lucide-react';
-import ErrorImg from "./components/ui/ErrorImg";
-import notFoundImg from '@/public/notFound.png'
 import PrimaryButton from "./components/ui/PrimaryButton";
 
 export default function NotFound() {
   return (
     <div className='flex flex-col justify-center items-center gap-3 w-full h-full'>
-      {/* <ErrorImg imagesrc={notFoundImg} /> */}
       <h2 className='text-6xl font-bold tracking-tighter text-onBackground px-5'>Página no encontrada...</h2>
       <p className="squircle max-w-xl p-6 rounded-lg text-pretty text-xl text-center leading-7 
        text-onSurface bg-surfaceContainerHigh">
