177013e649da67883ba8f635c07b7815796916dc
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 12:17:34 -0600

    refactor: rename Dashboard to Home and update layout with new components for improved structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import SearchBar from "./components/ui/SearchBar";
 
-export default async function Dashboard() {
+export default async function Home() {
 
   // throw new Error('500');
 
   return (
-    <div className="flex items-center justify-center w-full h-full text-3xl text-onPrimaryContainer">
+    <div className="flex px-20 py-12 w-full min-h-screen h-full text-3xl text-onPrimaryContainer">
+      {/* <SearchBar /> */}
+      <div className="flex flex-col gap-3">
+        <div className="size-56 rounded-2xl bg-primaryContainer">
+
+        </div>
+        
+        <div className="flex flex-col gap-2">
+          <span className="text-2xl font-semibold">Ola</span>
+          <p className="text-base font-light">Ola</p>
+          <span className="text-xl font-medium">$444</span>
+        </div>
+      </div>
     </div>
   );
 }
