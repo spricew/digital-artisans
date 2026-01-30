3149e42378ea071f2a260d006c3d2e784ce208c2
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 12:26:28 -0600

    refactor: update PrimaryCard component layout and styles for improved responsiveness and text display

--- app/components/ui/PrimaryCard.tsx
+++ app/components/ui/PrimaryCard.tsx
@@ -6,14 +6,14 @@ interface CardProps {
 
 export default function PrimaryCard({ title, description, price } : CardProps) {
     return (
-        <div className="flex flex-col gap-3">
-            <div className="size-56 rounded-2xl bg-primaryContainer">
+        <div className="flex flex-col gap-3 w-56">
+            <div className="w-full aspect-square rounded-2xl bg-primaryContainer">
                 {/* imagen */}
             </div>
 
-            <div className="flex flex-col gap-2">
-                <span className="text-2xl font-semibold">{title}</span>
-                <p className="text-base font-light">{description}</p>
+            <div className="flex flex-col gap-2 w-full">
+                <span className="text-2xl font-semibold line-clamp-3">{title}</span>
+                <p className="text-base font-light line-clamp-2">{description}</p>
                 <span className="text-xl font-medium">${price}</span>
             </div>
         </div>
