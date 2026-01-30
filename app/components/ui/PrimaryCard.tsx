9fbee1d160f3defe2afe49eb197af29b24c0248f
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 12:22:41 -0600

    feat: add PrimaryCard component for displaying product details with title, description, and price

---
+++ app/components/ui/PrimaryCard.tsx
@@ -0,0 +1,21 @@
+interface CardProps {
+    title: string;
+    description: string;
+    price: number;
+}
+
+export default function PrimaryCard({ title, description, price } : CardProps) {
+    return (
+        <div className="flex flex-col gap-3">
+            <div className="size-56 rounded-2xl bg-primaryContainer">
+                {/* imagen */}
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <span className="text-2xl font-semibold">{title}</span>
+                <p className="text-base font-light">{description}</p>
+                <span className="text-xl font-medium">${price}</span>
+            </div>
+        </div>
+    );
+}
