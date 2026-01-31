f00eea694492b3032b832597cf8f318cbfd0b84f
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 18:26:57 -0600

    feat: integrate FilterButton and PrimaryCard components into Home page for enhanced UI and product display

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import FilterButton from "./components/ui/FilterButton";
+import PrimaryCard from "./components/ui/PrimaryCard";
 import SearchBar from "./components/ui/SearchBar";
 
 export default async function Home() {
@@ -5,19 +7,18 @@ export default async function Home() {
   // throw new Error('500');
 
   return (
-    <div className="flex px-20 py-12 w-full min-h-screen h-full text-3xl text-onPrimaryContainer">
-      {/* <SearchBar /> */}
-      <div className="flex flex-col gap-3">
-        <div className="size-56 rounded-2xl bg-primaryContainer">
-
-        </div>
-        
-        <div className="flex flex-col gap-2">
-          <span className="text-2xl font-semibold">Ola</span>
-          <p className="text-base font-light">Ola</p>
-          <span className="text-xl font-medium">$444</span>
-        </div>
+    <div className="flex flex-col gap-8 px-20 py-12 w-full min-h-screen h-full text-3xl text-onPrimaryContainer">
+      <div className="flex gap-2">
+        <SearchBar />
+        <FilterButton />
       </div>
+
+      <PrimaryCard
+        title="Plato nuevo"
+        description="hola este es un nuevo producto que deberias probar"
+        price={300}
+      />
+
     </div>
   );
 }
