563d13c5755530e4e17418ab5d70acf1b8009be4
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 23:58:02 -0600

    refactor: update layout styles for improved body height and responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`flex flex-col h-screen antialiased`}>
+      <body className={`h-fit min-h-screen antialiased`}>
         <Navbar />
         {children}
         <FooterVariant />
