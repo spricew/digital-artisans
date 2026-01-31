484ffe825ee13539cf3bfbece5f88ea8ea0e659d
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 18:29:05 -0600

    feat: add click handler to FilterButton for enhanced interactivity

--- app/components/ui/FilterButton.tsx
+++ app/components/ui/FilterButton.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import { Filter } from 'lucide-react'
 export default function FilterButton() {
     return (
-        <button 
+        <button onClick={() => console.log('ola')}
         className="flex justify-center items-center h-full aspect-square rounded-full bg-primaryContainer
         cursor-pointer transition-transform duration-300 ease-out hover:scale-110">
             <Filter />
