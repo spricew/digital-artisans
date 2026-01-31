3f51740d1f47ee30d5f199ff5f71935370dcaf05
Heyder Medina <medinaheyder@gmail.com>
2026-01-30 18:27:13 -0600

    refactor: adjust SearchBar component styles for improved layout and spacing

--- app/components/ui/SearchBar.tsx
+++ app/components/ui/SearchBar.tsx
@@ -4,16 +4,16 @@ import { Search } from "lucide-react";
 export default function SearchBar() {
     return (
 
-        <div className="relative flex items-center w-full max-w-3xl rounded-full ring ring-outlineVariant
+        <div className="relative flex items-center py-3 w-full max-w-3xl rounded-full ring ring-outlineVariant
         bg-surfaceContainerLow text-onSurface text-xl">
-            <span className="flex justify-center items-center h-14 pl-5">
+            <span className="flex justify-center items-center pl-5">
                 <Search className="size-[1em] text-inherit z-10 opacity-50" />
             </span>
 
             <input
                 type="text"
                 placeholder="Buscar artículos"
-                className="w-full h-14 rounded-full pl-3.5 z-0 focus:outline-0"
+                className="rounded-full pl-3.5 z-0 focus:outline-0"
             />
         </div>
     );
